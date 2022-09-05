ca53aa960cface215d05000334729bca6cc69399
davidimbong <davidimbong.it@gmail.com>
2022-09-05 15:04:40 +0800

    used until instead of ..

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,7 @@
 fun main(args: Array<String>) {
     //SP 1
     var sum = 0
-    for(it in 1..999){
+    for(it in 1 until 1000){
         if ( it % 3 == 0 || it % 5 == 0)
             sum+=it
     }
