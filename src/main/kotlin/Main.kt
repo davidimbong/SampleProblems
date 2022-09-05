add1acaadad307635e046044ee1749cdff00a578
davidimbong <davidimbong.it@gmail.com>
2022-09-05 16:47:07 +0800

    sample problem 2
    Each new term in the Fibonacci sequence is generated by adding the previous two terms. By starting with 1 and 2, the first 10 terms will be:
    
    1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
    
    By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,25 @@
 fun main(args: Array<String>) {
     //SP 1
+//    var sum = 0
+//    for(it in 1 until 1000){
+//        if ( it % 3 == 0 || it % 5 == 0)
+//            sum+=it
+//    }
+//    println(sum)
+//
+    //SP 2
     var sum = 0
-    for(it in 1 until 1000){
-        if ( it % 3 == 0 || it % 5 == 0)
-            sum+=it
+    var x = 1
+    var y = 2
+    var z = 0
+    while(true){
+        if (y >= 4000000)
+            break
+        if(y % 2 == 0)
+            sum+=y
+        z = x + y
+        x = y
+        y = z
     }
     println(sum)
 }
