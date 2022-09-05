e2d3a6dcb81f344db17539531c3e8da8f3565a25
davidimbong <davidimbong.it@gmail.com>
2022-09-05 15:03:25 +0800

    sample problem 1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,7 @@
 fun main(args: Array<String>) {
-
     //SP 1
-    var sum:Int = 0
-    for(it in 1..1000){
+    var sum = 0
+    for(it in 1..999){
         if ( it % 3 == 0 || it % 5 == 0)
             sum+=it
     }
